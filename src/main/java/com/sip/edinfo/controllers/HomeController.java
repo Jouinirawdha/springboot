5fdee76a5a96cc34182bc98016a0df507fe989df
Jouinirawdha <jouinirawdha22@gmail.com>
2023-02-04 12:22:12 +0100

    fin add form

--- src/main/java/com/sip/edinfo/controllers/HomeController.java
+++ src/main/java/com/sip/edinfo/controllers/HomeController.java
@@ -4,10 +4,19 @@ import java.util.ArrayList;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
+	@PostMapping("/result")
+	@ResponseBody //crée une page web pour afficher "ok"
+	public String resultat(@RequestParam("nom")String n,@RequestParam("email")String e) {
+	return "Vous avez taper un nom : "+n +" un email: "+e;	
+	}
+	
 	@RequestMapping("/info")// ce que je tape dans l'url
 	public String info(Model model) {
 		String formation ="Fullstack";
@@ -17,6 +26,7 @@ public class HomeController {
 		names.add("Spring");
 		names.add("angular");
 		System.out.println("Méthode info");
+		
 		model.addAttribute("workshop",formation);
 		model.addAttribute("names",names);//envoyer à view on va passer par model
    return "home/info";//le nom de la vue sous template /home 
