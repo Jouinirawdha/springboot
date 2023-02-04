ca58e10e18b4cb0807f1623971ff6c621467c1cd
Jouinirawdha <jouinirawdha22@gmail.com>
2023-02-04 10:24:14 +0100

    add de css

--- src/main/java/com/sip/edinfo/controllers/HomeController.java
+++ src/main/java/com/sip/edinfo/controllers/HomeController.java
@@ -1,5 +1,7 @@
 package com.sip.edinfo.controllers;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +11,14 @@ public class HomeController {
 	@RequestMapping("/info")// ce que je tape dans l'url
 	public String info(Model model) {
 		String formation ="Fullstack";
+		
+		ArrayList <String> names = new ArrayList<>();
+		names.add("OCP");
+		names.add("Spring");
+		names.add("angular");
 		System.out.println("Méthode info");
 		model.addAttribute("workshop",formation);
+		model.addAttribute("names",names);//envoyer à view on va passer par model
    return "home/info";//le nom de la vue sous template /home 
 	}
 @RequestMapping("/affichage")
